7df56099526b4bc30b6f38a9e08bcf487e95b4eb
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-01-11 11:22:21 +0100

    feat: post endpoint and docs endpoint with redirect

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query, Redirect } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll() {
     return 'This action return all cats';
+  }
+
+  @Post()
+  create() {
+    return 'this action adds a cat';
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version: string) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
